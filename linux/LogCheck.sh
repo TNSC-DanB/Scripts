bc4c11bb0344312c81dde4443302d27e262fdf2e
TNSC-DanB <dburns@network-support.com>
2016-02-02 09:28:52 -0500

    Create LogCheck.sh
    Script to monitor a log file, and send an e-mail alert in the event that the log has not updated since last run.  Stores current status in a file, only sends e-mail on first alert after a clear status.

---
+++ linux/LogCheck.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+FILE=test.txt
+STATUSFILE=LogCheck.status
+MAILTO=dburns@network-support.com
+
+FILEHASH=$(cat $FILE.md5)
+STATUS=$(cat $STATUSFILE)
+NEWHASH=$(md5sum $FILE)
+MODTIME=$(stat -c %y $FILE | sed 's/$FILE//')
+
+if [ -e $STATUSFILE ];then
+	echo "StatusFile Exists"
+else
+	touch $STATUSFILE && echo "Created status file" 
+fi
+
+echo "Filehash is $FILEHASH NewHash is $NEWHASH"
+
+if [[ "$FILEHASH" == "$NEWHASH" ]]; then
+   echo "File not updated!"
+
+   if [[ "$STATUS" != "ALERT" ]]; then
+	echo "Previous status was not an alert! Send e-mail!"
+	echo "File $FILE on $HOSTNAME is the same since $MODTIME.  Please investigate!" | mail -s "Stale $FILE on $HOSTNAME" $MAILTO
+   else
+	echo "Previous status was alert, e-mail already sent."
+   fi
+   
+   echo "ALERT" > $STATUSFILE
+else
+   echo "Log updated, all clear"
+   echo "clear" > $STATUSFILE
+fi
+
+md5sum test.txt > test.txt.md5
